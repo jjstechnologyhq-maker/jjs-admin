d7d6a1bd2817cb159fa1c0a3cadbe7550cf0327a
Zacheus <segunzacheusi@gmail.com>
2026-09-15 12:12:51 +0100

    changes

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -11,7 +11,7 @@ export default function DashboardPage() {
         <div className="px-4 lg:px-6">
           <ChartAreaInteractive />
         </div>
-        <div className="hidden">
+        <div className="hidden ">
           <DataTable data={data} />
         </div>
       </div>
